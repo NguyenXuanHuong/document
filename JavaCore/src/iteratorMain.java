d773cbc71dae656c591f53f1bde49a0d13a39f53
xuanhuong <xuanhuongdhbk98@gmail.com>
2022-08-23 09:28:18 +0700

    iterator

--- JavaCore/src/iteratorMain.java
+++ JavaCore/src/iteratorMain.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class iteratorMain {
   public static void main(String[] args) {
 
-    List<String> list = List.of("a", "b","c");
-    System.out.println(list.getClass());
+    List<String> list = new ArrayList<>(List.of("A", "B", "C"));
+    Iterator<String> iterator = list.iterator();
+    while (iterator.hasNext()) {
+      String value = iterator.next();
+      System.out.println(value);
+    }
   }
 }

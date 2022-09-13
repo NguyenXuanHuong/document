981f5d517201f15a8194b25f4e35bfcc63b8edca
xuanhuong <xuanhuongdhbk98@gmail.com>
2022-09-13 22:06:22 +0700

    add all

--- AmigoMongoSpringBoot/src/main/java/com/example/AmigoMongoSpringBoot/AmigoMongoSpringBootApplication.java
+++ AmigoMongoSpringBoot/src/main/java/com/example/AmigoMongoSpringBoot/AmigoMongoSpringBootApplication.java
@@ -10,6 +10,10 @@ public class AmigoMongoSpringBootApplication {
 
 	public static void main(String[] args) {
 		System.out.println("port =============== "+System.getenv("PORT"));
+		System.out.println("username =============== "+System.getenv("USERNAME_DB"));
+		System.out.println("pwd =============== "+System.getenv("PASSWORD"));
+		System.out.println("SERVER-NAME =============== "+System.getenv("SERVER_NAME"));
+		System.out.println("HOST =============== "+System.getenv("HOST"));
 		SpringApplication.run(AmigoMongoSpringBootApplication.class, args);
 
 	}

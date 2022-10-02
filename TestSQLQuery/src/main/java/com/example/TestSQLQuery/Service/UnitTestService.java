bbaa04ddfae738ffd86af4794a950eb8746b56a2
xuanhuong <xuanhuongdhbk98@gmail.com>
2022-10-02 21:21:48 +0700

    m

--- TestSQLQuery/src/main/java/com/example/TestSQLQuery/Service/UnitTestService.java
+++ TestSQLQuery/src/main/java/com/example/TestSQLQuery/Service/UnitTestService.java
@@ -1,21 +1,28 @@
 package com.example.TestSQLQuery.Service;
 
 import com.example.TestSQLQuery.Repository.EmployeeRepository;
+import org.hibernate.jpamodelgen.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UnitTestService {
+
     @Autowired
-    EmployeeRepository employeeRepository;
+    MockService mockService;
+    @Autowired
+    MockService2 mockService2;
+
+    @Autowired
+    MockService2 mockService3;
 
     public String testService(){
-        try{
-            employeeRepository.getAllEmployee();
-            return "cde";
-        }catch (Exception e){
-            throw new RuntimeException("Abc");
-        }
 
+        //
+        String abc = "Abc";
+//        String s = mockService.mockMethod(abc) + mockService2.mockMethod2(abc);
+//        String s = mockService2.mockMethod2(abc);
+//        return abc;
+        return mockService.mockMethod(abc,"cde");
     }
 }
